578a3dc63e9b0c619339b216ccc47f42eb8077c3
wahaj <ahmad.wahaj04@gmail.com>
2022-10-31 03:37:02 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+alert("WELCOME");
+nationality = "PAKISTAN";
+var nationality = "PAKISTAN";
+alert (nationality)
+a = 10 
+b = 20
+c = 30 
+d = a + b + c
+alert (d)
+a = 50
+b = 100   
+c = 30
+d = a++ * --b / c 
+e = (a + b) / (c + d)
+f = a + b + c + d + e 
+alert (f)
+a++
+console.log( a )
